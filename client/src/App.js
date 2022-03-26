6a9269d3c3c49756ed88fc0645d3ed7e1514f18d
Dan Paxton <danpaxton47@gmail.com>
2022-03-26 13:29:42 -0400

    Updated file system

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,11 @@ import Icon from '@mui/material/Icon';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
 import { useState, useEffect } from 'react';
 
@@ -46,60 +49,134 @@ export const useLocalStorage = (key, defaultValue) => {
   return [value, setValue];
 };
 
+const baseUrl = "http://localhost:5000/sscript"
 
 const App = () => {
-  const [code, setCode] = useLocalStorage('code', 'func = () => "Hello World"; print(func());');
+  const [code, setCode] = useLocalStorage('code', '');
   const [output, setOutput] = useLocalStorage('out', '');
-  const [open, setOpen] = useState(false);
+  const [fileId, setFileId] = useLocalStorage('fileId', null);
+  const [fileTitle, setFileTitle] = useLocalStorage('fileTitle','Create new file.');
+  
+  const [fileList, setFileList] = useState([]);
+  const [openClear, setOpenClear] = useState(false);
+  const [openNewFile, setOpenNewFile] = useState(false);
+  const [fileName, setFileName] = useState("");
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const handleOpenClear = () => setOpenClear(true);
+  const handleCloseClear = () => setOpenClear(false);
+  const handleClear = () => { setCode(""); setOutput(""); setOpenClear(false) };
 
-  const handleClear =() => {
-      setOpen(false);
-      setCode(""); 
-      setOutput("");
+
+  const onNameChange = (f) => setFileName(f.target.value);
+  const handleOpenNewFile = () => setOpenNewFile(true);
+  const handleCloseNewFile = () => { setFileName(""); setOpenNewFile(false) };
+  const handleNewFile = () => { newFile(fileName); setFileName(""); setOpenNewFile(false) };
+
+
+
+  const validInput = () => {
+    const name_arr = fileName.split('.');
+    if (name_arr.length > 2 || name_arr[1] !== 'sscript') {
+      return { msg: "File name must end in '.sscript'.", isValid: false };
+    }
+    if (name_arr[1].length > 100) {
+      return { msg: "Max character limit exceeded.", isValid: false };
+    }
+    return { isValid: true };
+  }
+
+
+  const fetchFiles = async () => {
+    const data =  await axios.get(`${baseUrl}/fetch-files`)
+    const { files } =  data.data;
+
+    setFileList(files);
   }
 
   const runCode = () => {
     axios
-      .post("http://127.0.0.1:5000/sscript", parseProgram(code))
+      .post(`${baseUrl}/interp`, parseProgram(code))
       .then(res =>  setOutput(res.data))
   }
+
+  const newFile = async (title) => {
+    try {
+      const { data } = await axios.post(`${baseUrl}/new-file`, {title: title, source_code: ""})
+      setFileList([...fileList, data]);
+      if (fileList.length > 0) {
+        saveFile(fileId, code);
+      }
+      loadFile(data.id);
+    } catch(err) {
+      console.error(err.message);
+    }
+  }
+
+  const deleteFile = async (title, id) => {
+    try {
+      await axios.delete(`${baseUrl}/fetch-file/${id}`)
+      setFileList(fileList.filter(f => f.id !== id));
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  const loadFile = async (id) => {
+    try {
+      const { data } = await axios.get(`${baseUrl}/fetch-file/${id}`)
+      if (fileList.length > 0) {
+        saveFile(fileId, code);
+      }
+      setFileId(data.id);
+      setFileTitle(data.title);
+      setCode(data.source_code);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  const saveFile = async (id, source_code) => {
+   try {
+      await axios.put(`${baseUrl}/update-file/${id}`, { source_code })
+    } catch(err) {
+      handleOpenNewFile();
+    }
+  }
+
+  useEffect(() => {
+    fetchFiles();
+  }, [])
 
   const downloadCode = () => {
     const element = document.createElement("a");
     const file = new Blob([code], { type: "text/plain" });
     element.href = URL.createObjectURL(file);
-    element.download = "slime_lang.txt";
+    element.download = "sscript.txt";
     document.body.appendChild(element);
     element.click();
   };
 
   return (
-    <div class="App">
+    <div className="App">
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet"></link>
-      <header class="App-header">
-        <div class="Title">
+        <div className="Title">
           Simple-Script IDE </div>
         <ThemeProvider theme={theme}>
-        <div class='codeBox'>
+        <div className='codeBox'>
         <Button variant="text" color="primary" onClick={runCode}> <Icon>play_arrow</Icon>Run</Button>
+        <Button variant="text" color="primary" onClick={() => saveFile(fileId, code)}> <Icon>playlist_add_check</Icon>Save</Button>
         <Button variant="text" color="primary" onClick={downloadCode}> <Icon>download</Icon>Download</Button>
-          <Button variant="text" color="secondary" onClick={handleClickOpen}> <Icon>clear</Icon>Clear </Button>
-          <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title">
-            <DialogTitle id="alert-dialog-title">{"Clear all code?"}</DialogTitle>
-            <DialogActions>
-              <Button variant="contained" color="primary" onClick={handleClose}>Cancel</Button>
-              <Button variant="contained" color="secondary" onClick={handleClear}>Clear</Button>
-            </DialogActions> 
-          </Dialog>
-        <CodeMirror className='CodeMirror'
+        <Button variant="text" color="secondary" onClick={handleOpenClear}> <Icon>clear</Icon>Clear </Button>
+        <div className='fileName'>{fileTitle}</div>
+        <Dialog open={openClear} aria-labelledby="alert-dialog-title">
+          <DialogTitle id="alert-dialog-title">{"Clear all code?"}</DialogTitle>
+          <DialogActions>
+            <Button variant="contained" color="primary" onClick={handleCloseClear}>Cancel</Button>
+            <Button variant="contained" color="secondary" onClick={handleClear}>Clear</Button>
+          </DialogActions> 
+        </Dialog>
+        <CodeMirror 
           value={code}
           options={{
             theme: "colorforth",
@@ -111,11 +188,34 @@ const App = () => {
           }}
         />
         </div>
+        <div className="fileBox">
+          <Button variant="text" color="primary" onClick={handleOpenNewFile}> <Icon>post_add</Icon>New file</Button>
+          <Dialog open={openNewFile} aria-labelledby="alert-dialog-title"> 
+            <DialogActions>
+              <TextField id="standard-basic" defaultValue={fileName}  label="Enter file name." variant="standard" onChange={onNameChange}/>
+              <Button variant="contained" color="primary" onClick={handleNewFile}>Enter</Button>
+              <Button variant="contained" color="secondary" onClick={handleCloseNewFile}>Cancel</Button>
+            </DialogActions> 
+          </Dialog>
+          <List dense={true} color="primary">
+              {fileList.map(file => (
+                <ListItem key={file.id}
+                disablePadding
+                  secondaryAction={
+                    <IconButton edge="end" color="secondary" aria-label="delete" onClick={() => deleteFile(file.title, file.id)}>
+                      <Icon>delete</Icon>
+                    </IconButton>
+                  }
+                >
+                <Button size='small' style={{textTransform:'none'}} variant="outlined" fullWidth={true} color="primary" onClick={() => loadFile(file.id)}>{file.title}</Button>
+                </ListItem>)
+              )}
+            </List>
+        </div>
         <div className='outputBox'>
           output: <p style={{color: 'white'}}>{output}</p>
         </div> 
         </ThemeProvider>
-      </header>
     </div>
   );
 }
--- server/base.py
+++ server/base.py
@@ -1,12 +1,71 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 
 api = Flask(__name__)
+api.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:G=scB(5,(-4g"9A<@localhost/sscript-ide'
+db = SQLAlchemy(api)
 CORS(api)
 
-@api.route('/sscript', methods=['POST'])
-def get_code():
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.VARCHAR(100), nullable=False)
+    source_code = db.Column(db.TEXT, nullable=False)
+
+    def __repr__(self):
+        return f"{self.title}: {self.source_code}"
+
+    def __init__(self, title, source_code):
+        self.title = title
+        self.source_code = source_code
+
+
+def format_file(file):
+    return {
+        "id": file.id,
+        "title": file.title,
+        "source_code": file.source_code,
+    }
+
+# Save file
+@api.route('/sscript/new-file', methods=['POST'])
+def save_file():
+    data = request.get_json()
+    file = File(data['title'], data['source_code'])
+    db.session.add(file); db.session.commit()
+    return format_file(file)
+
+
+@api.route('/sscript/fetch-files', methods=['GET'])
+def fetch_files():
+    files = File.query.order_by(File.id.asc()).all()
+    return { 'files': [format_file(file) for file in files] } 
+
+
+# Get or Delete file
+@api.route('/sscript/fetch-file/<id>', methods=['GET', 'DElETE'])
+def fetch_file(id):
+    file = File.query.filter_by(id=id).one()
+    if request.method == 'GET':
+        return format_file(file)
+    else:
+        db.session.delete(file); db.session.commit()
+        return "File deleted."
+
+# Update file
+@api.route('/sscript/update-file/<id>', methods=['PUT'])
+def update_file(id):
+    file = File.query.filter_by(id=id)
+    file.update(dict(source_code=request.json['source_code']))
+    db.session.commit()
+    return format_file(file.one())
+
+
+# Interp parsed code
+@api.route('/sscript/interp', methods=['POST'])
+def interp_code():
     parsedCode = request.get_json()
 
     if parsedCode['kind'] != 'ok':
@@ -16,4 +75,4 @@ def get_code():
 
 
 if __name__ == "__main__":
-    api.run(debug=True)
+    api.run(debug=True, port=5000)
